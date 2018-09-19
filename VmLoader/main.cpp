de014af746dd3caf32232a0d334dec3548a04ee9
hzqst <113660872@qq.com>
2018-09-19 16:54:52 +0800

    remove unicorn

--- VmLoader/main.cpp
+++ VmLoader/main.cpp
@@ -2,6 +2,15 @@
 
 extern "C"
 {
+	_Use_decl_annotations_ void *UtilGetSystemProcAddress(
+		const wchar_t *proc_name) {
+		PAGED_CODE();
+
+		UNICODE_STRING proc_name_U = {};
+		RtlInitUnicodeString(&proc_name_U, proc_name);
+		return MmGetSystemRoutineAddress(&proc_name_U);
+	}
+
 	_Use_decl_annotations_ static void DriverUnload(
 		PDRIVER_OBJECT driver_object) {
 		UNREFERENCED_PARAMETER(driver_object);
@@ -14,6 +23,12 @@ extern "C"
 		PUNICODE_STRING registry_path) {
 		UNREFERENCED_PARAMETER(registry_path);
 		PAGED_CODE();
+
+		auto pfnNtQuerySystemInformation = UtilGetSystemProcAddress(L"NtQuerySystemInformation");
+
+		DbgPrint("NtQuerySystemInformation found at %p", pfnNtQuerySystemInformation);
+
+
 
 		driver_object->DriverUnload = DriverUnload;
 
