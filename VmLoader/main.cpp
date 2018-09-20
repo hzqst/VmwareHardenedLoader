90c6fb1940079c984c4955d56b8afada63784692
hzqst <113660872@qq.com>
2018-09-20 11:24:31 +0800

    77777777

--- VmLoader/main.cpp
+++ VmLoader/main.cpp
@@ -682,7 +682,7 @@ extern "C"
 			if (!find)
 				break;
 
-			memset(find, '6', SigLength);
+			memset(find, '7', SigLength);
 			search_begin = (PUCHAR)find + SigLength;
 			search_size = (PUCHAR)FirmwareBuffer + FirmwareBufferLength - search_begin;
 		}
