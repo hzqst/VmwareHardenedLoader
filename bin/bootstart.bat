691a0365cbff3975082c2fa5a6c30c14bdde45a0
hzqst <113660872@qq.com>
2018-12-05 14:47:38 +0800

    1

--- bin/bootstart.bat
+++ bin/bootstart.bat
@@ -1,8 +1,4 @@
 copy vmloader.sys "C:\vmloader.sys"
-copy hidden.sys "C:\hidden.sys"
-copy hiddentests.exe "C:\hiddentests.exe"
-sc create vmloader binPath= "\??\c:\vmloader.sys" type= "kernel" start="system"
+sc create vmloader binPath= "\??\c:\vmloader.sys" type= "kernel"
 sc start vmloader
-sc create hidden binPath= "\??\c:\hidden.sys" type= "kernel" start="system"
-sc start hidden
-reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v HiddenTests /t REG_SZ /d C:\hiddentests.exe /f
+reg delete HKLM\HARDWARE\ACPI\DSDT\PTLTD_
